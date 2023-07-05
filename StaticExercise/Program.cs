4e06ce71655dd255899fe2aa1bb266aad08c6e8f
Bryan Levin <bryanlevin72@gmail.com>
2023-07-05 12:11:28 -0700

    Exercise 1

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double celsius = TempConverter.FarenheitToCelsius(32);
+            double farenheit = TempConverter.CelsiusToFarenheit(0);
+
+            Console.WriteLine($"{farenheit} degrees Farenheit is equal to {celsius} degrees celsius.");
+            Console.WriteLine($"{celsius} degrees Celsius is equal to {farenheit} degrees Farenheit.");
+ 
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		public static double FarenheitToCelsius(double farenheit)
+		{
+			double celsius = (farenheit - 32) / 1.8;
+			return celsius;
+		}
+
+		public static double CelsiusToFarenheit(double celsius)
+		{
+			double farenheit = ((celsius * 9) / 5 + 32);
+			return farenheit;
+		}
+	}
+}
+
